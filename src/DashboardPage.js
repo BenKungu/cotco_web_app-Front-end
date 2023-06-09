44d698e28a153f23ce257a548ba9bebc8c7fc36f
BenKungu <benwaweru2013@gmail.com>
2023-06-09 10:13:06 +0300

    first commit

---
+++ src/DashboardPage.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./DashboardPage.css"; // Import the CSS file for styling
+
+const DashboardPage = () => {
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-header">
+        <h2>Welcome to Cotco Admin</h2>
+        <div className="header-buttons">
+          <button>Farmers</button>
+          <button>Sales</button>
+          <button>Users</button>
+          <button>Collections</button>
+          <button>Vouchers</button>
+        </div>
+      </div>
+      <table className="dashboard-table">
+        <thead>
+          <tr>
+            <th>Farmer</th>
+            <th>Phone</th>
+            <th>Sales</th>
+            <th>Vouchers</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>074774488</td>
+            <td>30000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Kelvin</td>
+            <td>07248899</td>
+            <td>40000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Martin</td>
+            <td>07348888</td>
+            <td>50000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Martin</td>
+            <td>07348888</td>
+            <td>50000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Martin</td>
+            <td>07348888</td>
+            <td>50000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Kelvin</td>
+            <td>07248899</td>
+            <td>40000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Kelvin</td>
+            <td>07248899</td>
+            <td>40000</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Kelvin</td>
+            <td>07248899</td>
+            <td>40000</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ src/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./LoginPage.css"; // Import the CSS file for styling
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform login logic here, such as making an API call
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Enter your username"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Enter your password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
